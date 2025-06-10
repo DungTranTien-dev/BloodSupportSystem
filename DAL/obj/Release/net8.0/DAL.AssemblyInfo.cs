6e9665600d7b406733719cc5f5de5fc9d7458e75
FuHuaLover29 <0152ooms@gmail.com>
2025-06-10 08:51:32 +0700

    update git ignore and merge with ViHao
    update

--- DAL/obj/Release/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Release/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99bddbe43eeba0d3f78183ec8df8827023c40c0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a98be877c83e337385fb3edbcdde16e49ed10aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
