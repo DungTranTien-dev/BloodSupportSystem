d9feb3e17a6dc72bc4363a48f46956fb2cdab547
hungnqse182498 <hungnqse182498@fpt.edu.vn>
2025-06-13 10:42:54 +0700

    ADD

---
+++ FE/BloodSystemFE-main/Models/Blood.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Blood
+    {
+        public Guid BloodId { get; set; }
+        public string BloodName { get; set; }
+        public int TotalQuantity { get; set; }
+        //adasd
+    }
+}
